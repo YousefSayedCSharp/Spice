8abd051c20f3fdb0c6c4225ebba6f3070a0dd0fb
YousefSayedCSharp <yousef.elmsry@gmail.com>
2021-06-23 19:05:37 +0200

    add coupon controller and views and coupon model and finshed

---
+++ Spice/Areas/Admin/Controllers/CouponsController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+using Spice.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CouponsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CouponsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        string CreateEditButtons = "Create";
+        public async Task<IActionResult> Index()
+        {
+            //this is view edit and create name
+            ViewData["CreateEditButtons"] = CreateEditButtons;
+
+            var coupons = await _context.Coupons.ToListAsync();
+
+            return View(coupons);
+        }
+
+        public async Task<IActionResult> Create(int? id)
+        {
+            if (id == null)
+                return BadRequest();
+
+            var coupon = await _context.Coupons.FindAsync(id);
+            if (id > 0)
+            {
+                if (coupon == null)
+                    return NotFound();
+
+                return View(coupon);
+            }
+
+            return View(new Coupon { });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Coupon model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            if(model.CouponType != "Percent"||model.CouponType!="Doller")
+            {
+                ModelState.AddModelError("CouponType","Please Select the coupon type of list!");
+                return View(model);
+            }
+
+            var files = HttpContext.Request.Form.Files;
+
+            if(files.Count>0)
+            {
+                var fs = files[0].OpenReadStream();
+                var ms = new MemoryStream();
+                fs.CopyTo(ms);
+                model.Picture = ms.ToArray();
+            }
+
+            if (model.Id > 0)
+                _context.Update(model);
+            else
+                await _context.AddAsync(model);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> details(int? id)
+        {
+            if (id == null)
+                return BadRequest();
+
+            var coupon = await _context.Coupons.FindAsync(id);
+            if (coupon == null)
+                return NotFound();
+
+            ViewData["CreateEditButtons"] = CreateEditButtons;
+
+            return View(coupon);
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return BadRequest();
+
+            var coupon = await _context.Coupons.FindAsync(id);
+            if (coupon == null)
+                return NotFound();
+
+            _context.Coupons.Remove(coupon);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+    }//end c@section Scripts{lass
+}//end main
---
+++ Spice/Models/Coupon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name="Coupon Type")]
+        public string CouponType { get; set; }
+
+        public enum ECouponType {Percent=0,Doller=1}
+
+        public double Discount { get; set; }
+
+        [Display(Name = "Minimum Ammount")]
+        public double MinimumAmmount { get; set; }
+
+        public byte[] Picture { get; set; }
+
+        [Required]
+        [Display(Name="Is Active")]
+        public bool IsActive { get; set; }
+    }
+}
