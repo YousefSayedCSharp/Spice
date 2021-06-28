6b17035b30c2263315ab4534c77e6914402f2156
YousefSayedCSharp <yousef.elmsry@gmail.com>
2021-06-28 15:53:46 +0200

    Add home index page lecture 6

---
+++ 0/Spice/ViewModels/MenuItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Spice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.ViewModels
+{
+    public class MenuItemViewModel
+    {
+        public MenuItem menuItem { get; set; }
+        public IEnumerable<Category> categoriesList { get; set; }
+        public IEnumerable<SubCategory> subCategoriesList { get; set; }
+    }
+}
